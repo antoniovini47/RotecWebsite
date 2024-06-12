7c6b74f42b861f3cb29ee80934ec8d3bdebd2a70
Antonio Oliveira <antoniovini47@gmail.com>
2024-06-11 21:50:27 -0300

    Profile update.

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,44 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello, tom!</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>Count is {count}</button>
-        <p>You are a amazing person!!!</p>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Hello, fellow!</h1>
+        <p>
+          <button onClick={() => (window.location.href = "https://github.com/antoniovini47")}>
+            GitHub
+          </button>
+        </p>
+        <p>
+          <button onClick={() => (window.location.href = "https://linkedin.com/in/antoniovini47")}>
+            LinkedIn
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() =>
+              (window.location.href =
+                "https://play.google.com/store/apps/details?id=com.rotech.dezeroacinquenta")
+            }>
+            Android App: Zero a Cinquenta
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() =>
+              (window.location.href =
+                "https://play.google.com/store/apps/details?id=com.rotech.examepsicotecnico")
+            }>
+            Android App: Exame Psicotécnico
+          </button>
+        </p>
       </div>
     </>
   );
